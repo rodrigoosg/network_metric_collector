6760c78dec468f986214d4aaf6ff266a66161b36
Rodrigo de Oliveira S. Guimarães <rodrigo@agileoperations.com.br>
2016-11-10 15:19:02 -0200

    Added Scheduler lib instead of Sleep function;

--- collector.py
+++ collector.py
@@ -2,15 +2,24 @@
 
 from nmc.procnetdev import ProcNetDev
 import time
+from time import gmtime, strftime
 import sys
+import schedule
+
+last_bytes = 0
+
+def job():
+	global last_bytes
+	pnd = ProcNetDev()
+	tmp_bytes = last_bytes
+	last_bytes = pnd['eth0']['transmit']['bytes']
+	delta_bytes = last_bytes - tmp_bytes 
+	now = strftime("%Y/%m/%dT%H:%M:%S", gmtime())
+	print "Time: %s | Bytes Transmitted: %s B/s" % (now, int(delta_bytes))
+	sys.stdout.flush()
 
 if __name__ == "__main__":
-
+	schedule.every(1).minute.do(job)
 	while True:
-		pnd = ProcNetDev()
-		last_time  = time.time()
-		last_bytes = pnd['eth0']['receive']['bytes'] 
+		schedule.run_pending()
 		time.sleep(1)
-	    now_bytes = pnd['eth0']['receive']['bytes']
-		print "Bytes Received: %s B/s" % (int(now_bytes) - int(last_bytes))
-		sys.stdout.flush()
